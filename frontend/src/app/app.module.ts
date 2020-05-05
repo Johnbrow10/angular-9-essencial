5ef6c4c7182095b9eb3daf790271c07992e612ea
Johnatan Dos Santos Silva <Iroman250@gmail.com>
2020-05-04 22:20:43 -0300

    add Diretivas de atributos e esilizando product

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -17,6 +17,7 @@ import {MatListModule} from  '@angular/material/list';
 import { HomeComponent } from './views/home/home.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 import { ProductCreateComponent } from './components/product/product-create/product-create.component';
+import { RedDirective } from './directives/red.directive';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import { ProductCreateComponent } from './components/product/product-create/prod
     NavComponent,
     HomeComponent,
     ProductCrudComponent,
-    ProductCreateComponent
+    ProductCreateComponent,
+    RedDirective
   ],
   imports: [
     BrowserModule,
