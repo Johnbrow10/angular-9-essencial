40562d71daeff506f4ae56ba9f201e9611ab002e
Johnatan Dos Santos Silva <Iroman250@gmail.com>
2020-06-02 17:04:02 -0300

    Adicionando Metodo update e incrementando asssim dando para atualizar os dados

--- frontend/src/app/components/product/product-update/product-update.component.ts
+++ frontend/src/app/components/product/product-update/product-update.component.ts
@@ -1,4 +1,7 @@
+import { Router, ActivatedRoute } from '@angular/router';
+import { ProductService } from './../product.service';
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../product.model';
 
 @Component({
   selector: 'app-product-update',
@@ -7,9 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductUpdateComponent implements OnInit {
 
-  constructor() { }
+
+  product: Product;
+
+  constructor(private productService: ProductService,
+    private router: Router,
+    private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    // para carregar a pagina o from ja ira vim com os dados gracas a captura do ID
+    const id = this.route.snapshot.paramMap.get('id')
+    this.productService.readById(id).subscribe(product => {
+      this.product = product;
+    });
+  }
+
+  //  o metodo UpdateProduct utilizando o update feito na service e sobrescrevendo gracaos ao metodo observable
+  //  e depois manda mensagem gracas ao metodo showMessage e logo depois manda de volta para tabela.
+  updateProduct(): void {
+    this.productService.update(this.product).subscribe(() => {
+      this.productService.ShowMessage('Produto atualizado com sucesso!!')
+      this.router.navigate(['/products']);
+    });
+
+  }
+
+  Cancel(): void {
+    this.router.navigate(['/products']);
   }
 
 }
